351bfd98dfd43e27dce9b946e83a4bd370745710
Bros G <106735266+BrosG@users.noreply.github.com>
2023-05-31 17:50:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// Import necessary modules
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
 // Import routes
 const briefRoutes = require('./routes/briefRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 
 // Initialize express
 const app = express();
@@ -16,9 +16,14 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware for Cross-origin resource sharing
 app.use(cors());
 
+// Middleware to serve static files
+app.use(express.static('public'));
+
 // Use routes
 app.use('/createBrief', briefRoutes);
+app.use('/chat', chatRoutes);
 
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
